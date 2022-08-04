15f157695bf9f79c658d069195e4f4a26aee4662
Lora <lorat1403@gmail.com>
2022-08-04 19:28:56 +0300

    trbltest

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,9 @@
 import { Notify } from 'notiflix';
+const URL = 'https://restcountries.com/v3.1/name/';
 
-const URL =
-  'https://restcountries.com/v3.1/name?fields=name.official,capital,population,flags.svg,languages';
-
-export function fetchCountries(e) {
-  e.preventDefault();
-  fetch(URL)
+export function fetchCountries(name) {
+  let chosenCountry = `${URL}${name}?fields=name.official,capital,population,flags.svg,languages`;
+  fetch(chosenCountry)
     .then(response => response.json())
     .then(data => {
       if (data.length > 10) {
@@ -17,37 +15,39 @@ export function fetchCountries(e) {
       if (data.length >= 2 && data.length <= 10) {
         insertContent(data);
       }
-      if (data.length == 1) {
+      if (data.length === 1) {
         insertUnicItem(data);
       }
-      console.log('data', data);
     })
     .catch(error => {
       Notify.failure('Oops, there is no country with that name');
       refs.list.innerHTML = '';
     });
-}
 
-const createListItem = item => `<li><span>${item.URL.flags.svg}</span>
- ${item.URL.name.official}
+  const createListItem =
+    item => `<li><span>${item.chosenCountry.flags.svg}</span>
+ ${item.chosenCountry.name.official}
 </li>`;
-const createUnicItem = item => `<li>
-<p><b>Capital</b>: ${item.URL.capital}</p>
-<p><b>Population</b>: ${item.URL.population}</p>
-<p><b>Languages</b>: ${item.URL.languages}</p></li>`;
+  const createUnicItem =
+    item => `<h2><span>${item.chosenCountry.flags.svg}</span>
+ ${item.chosenCountry.name.official}</h2>
+<p><b>Capital</b>: ${item.chosenCountry.capital}</p>
+<p><b>Population</b>: ${item.chosenCountry.population}</p>
+<p><b>Languages</b>: ${item.chosenCountry.languages}</p>`;
 
-const generateUnicItem = array =>
-  array ? array.reduce((acc, item) => acc + createUnicItem(item), '') : '';
+  const generateUnicItem = array =>
+    array ? array.reduce((acc, item) => acc + createUnicItem(item), '') : '';
 
-const generateContent = array =>
-  array ? array.reduce((acc, item) => acc + createListItem(item), '') : '';
+  const generateContent = array =>
+    array ? array.reduce((acc, item) => acc + createListItem(item), '') : '';
 
-const insertContent = array => {
-  const result = generateContent(array);
-  refs.list.insertAdjacentHTML('beforeend', result);
-};
+  const insertContent = array => {
+    const result = generateContent(array);
+    refs.list.insertAdjacentHTML('beforeend', result);
+  };
 
-const insertUnicItem = array => {
-  const result = generateUnicItem(array);
-  refs.list.insertAdjacentHTML('beforeend', result);
-};
+  const insertUnicItem = array => {
+    const result = generateUnicItem(array);
+    refs.divInfo.insertAdjacentHTML('beforeend', result);
+  };
+}
