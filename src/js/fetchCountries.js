6fae4546383ad70d4421bb384b96a373e895aa76
Lora <lorat1403@gmail.com>
2022-08-20 17:17:49 +0300

    upderror

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,11 +4,11 @@ import { cleanMarkup } from './renderContent';
 export function fetchCountries(name) {
   const chosenCountry = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
 
-  return fetch(chosenCountry)
-    .then(response => response.json())
-    .catch(error => {
-      if (error === 'Error 404') {
-        Notify.failure('Oops, there is no country with that name');
-      }
-    });
+  return fetch(chosenCountry).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    } else {
+      return response.json();
+    }
+  });
 }
